4e81a2b0cda6834e88247bec2730500829dfcc57
Filipe Câncio <filipe.cancio@outlook.com>
2025-04-27 15:14:47 -0300

    Initial Commit

---
+++ swift-ui-sandbox/swift_ui_sandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_sandboxApp.swift
+//  swift-ui-sandbox
+//
+//  Created by Filipe Cancio on 27/04/25.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_sandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
